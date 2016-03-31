43f38d989b36bfd6c68913f2f29b173596a3d2dc
tbsmatt <matthew.yeow@tinkerbox.com.sg>
2016-03-31 21:33:38 +0800

    Implement #link_to_add_nested_value helper in ActionView::Cocoon for handling value objects, analogous to Cocoon's #link_to_add_association.

--- lib/value_objects/action_view/cocoon.rb
+++ lib/value_objects/action_view/cocoon.rb
@@ -5,6 +5,25 @@ module ValueObjects
 
     module Cocoon
 
+      def link_to_add_nested_value(name, f, attribute, value_class, html_options = {}, &block)
+        render_options = html_options.delete(:render_options) || {}
+        partial = html_options.delete(:partial)
+        wrap_object = html_options.delete(:wrap_object)
+        form_name = html_options.delete(:form_name) || 'f'
+        count = html_options.delete(:count).to_i
+        new_object = value_class.new
+        new_object = wrap_object.call(new_object) if wrap_object
+
+        html_options[:class] = [html_options[:class], 'add_fields'].compact.join(' ')
+        html_options[:'data-association'] = attribute
+        html_options[:'data-association-insertion-template'] = CGI.escapeHTML(render_association(attribute, f, new_object, form_name, render_options, partial)).html_safe
+        html_options[:'data-count'] = count if count > 0
+
+        name = capture(&block) if block_given?
+        dummy_input = tag(:input, type: 'hidden', name: "#{f.object_name}[#{attribute}_attributes][-1][_]")
+        content_tag(:a, name, html_options) + dummy_input
+      end
+
       def link_to_remove_nested_value(name, html_options = {}, &block)
         wrapper_class = html_options.delete(:wrapper_class)
 
--- spec/value_objects/action_view/cocoon_spec.rb
+++ spec/value_objects/action_view/cocoon_spec.rb
@@ -11,7 +11,124 @@ RSpec.describe ValueObjects::ActionView::Cocoon do
   let(:link_text) { link.text }
   let(:link_href) { link.attribute('href')&.value }
   let(:link_class) { link.attribute('class')&.value }
+  let(:link_association) { link.attribute('data-association')&.value }
+  let(:link_association_insertion_template) { link.attribute('data-association-insertion-template')&.value }
+  let(:link_data_count) { link.attribute('data-count')&.value }
   let(:link_wrapper_class) { link.attribute('data-wrapper-class')&.value }
+  let(:input) { body.at('input') }
+  let(:input_type) { input.attribute('type')&.value }
+  let(:input_name) { input.attribute('name')&.value }
+
+  describe '#link_to_add_nested_value' do
+
+    shared_examples_for 'link_to_add' do |options|
+
+      it 'renders the link correctly' do
+        aggregate_failures do
+          expect(body.children).to contain_exactly(link, input)
+          expect(link_text).to eq(options[:text])
+          expect(link_class).to eq(options[:class])
+          expect(link_association).to eq(options[:association])
+          expect(link_association_insertion_template).to eq(template)
+          expect(link_data_count).to eq(options[:count])
+          expect(link_href).to eq(nil)
+          expect(input_type).to eq('hidden')
+          expect(input_name).to eq(input_name)
+        end
+      end
+
+    end
+
+    before { expect(test_view).to receive(:render_association).and_return(template) }
+    let(:template) { '<div class="nested_fields"></div>' }
+
+    let(:html) { test_view.link_to_add_nested_value(*args, &block) }
+    let(:args) { [name, form, attribute, value_class] + [options].compact }
+    let(:name) { nil }
+    let(:form) { double(object_name: 'form[test_record]') }
+    let(:attribute) { :test_values }
+    let(:value_class) { ValueObjects::Value }
+    let(:options) { nil }
+    let(:block) { nil }
+    let(:input_name) { "#{form.object_name}[#{attribute}_attributes][-1][_]" }
+
+    include_examples 'link_to_add', text: '', class: 'add_fields', association: 'test_values'
+
+    context 'with name' do
+
+      let(:name) { 'Some text ...' }
+
+      include_examples 'link_to_add', text: 'Some text ...', class: 'add_fields', association: 'test_values'
+
+      context 'and block' do
+
+        let(:block) { -> { 'Block text' } }
+
+        include_examples 'link_to_add', text: 'Block text', class: 'add_fields', association: 'test_values'
+
+      end
+
+    end
+
+    context 'with class' do
+
+      let(:options) { { class: classes } }
+      let(:classes) { 'foo bar' }
+
+      include_examples 'link_to_add', text: '', class: 'foo bar add_fields', association: 'test_values'
+
+      context 'as an array' do
+
+        let(:classes) { %w( foo bar ) }
+
+        include_examples 'link_to_add', text: '', class: 'foo bar add_fields', association: 'test_values'
+
+      end
+
+    end
+
+    context 'with count' do
+
+      let(:options) { { count: 2 } }
+
+      include_examples 'link_to_add', text: '', class: 'add_fields', association: 'test_values', count: '2'
+
+    end
+
+    context 'with wrap_object' do
+
+      let(:options) { { wrap_object: wrapper } }
+      let(:wrapper) do
+        double.tap do |d|
+          expect(d).to receive(:call) do |obj|
+            expect(obj).to be_a(ValueObjects::Value)
+          end.once
+        end
+      end
+
+      include_examples 'link_to_add', text: '', class: 'add_fields', association: 'test_values'
+
+    end
+
+    context 'with other attribute' do
+
+      let(:attribute) { 'foobars' }
+
+      include_examples 'link_to_add', text: '', class: 'add_fields', association: 'foobars'
+
+    end
+
+    context 'with all args' do
+
+      let(:name) { 'text' }
+      let(:attribute) { 'value' }
+      let(:options) { { class: 'bar foo', count: 3, wrap_object: double(call: nil) } }
+
+      include_examples 'link_to_add', text: 'text', class: 'bar foo add_fields', association: 'value', count: '3'
+
+    end
+
+  end
 
   describe '#link_to_remove_nested_value' do
 
