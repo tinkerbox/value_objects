80aa8065f9a7bb585204bdc8b03692611783cef4
tbsmatt <matthew.yeow@tinkerbox.com.sg>
2016-05-24 11:53:50 +0800

    Extract JSON serialization logic from ValueObjects::Value out into ValueObjects::ActiveRecord.

--- lib/value_objects/active_record.rb
+++ lib/value_objects/active_record.rb
@@ -8,12 +8,37 @@ module ValueObjects
     end
 
     def value_object(attribute, value_class)
-      serialize(attribute, value_class)
+      coder =
+        case column_for_attribute(attribute).type
+        when :string, :text
+          JsonCoder.new(value_class)
+        else
+          value_class
+        end
+      serialize(attribute, coder)
       validates_with(::ValueObjects::ValidValidator, attributes: [attribute])
       setter = :"#{attribute}="
       define_method("#{attribute}_attributes=") do |attributes|
         send(setter, value_class.new(attributes))
       end
+    end
+
+    class JsonCoder
+
+      EMPTY_ARRAY = [].freeze
+
+      def initialize(value_class)
+        @value_class = value_class
+      end
+
+      def load(value)
+        @value_class.load(JSON.load(value) || EMPTY_ARRAY) if value
+      end
+
+      def dump(value)
+        value.to_json if value
+      end
+
     end
 
   end
